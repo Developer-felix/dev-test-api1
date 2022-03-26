2d7cd67f1a12b1c20f4145e7845081b5533e26b7
Developer-felix <onjombafelix1@gmail.com>
2022-03-26 07:27:09 -0700

    Protected the routes

--- routes/api.php
+++ routes/api.php
@@ -16,18 +16,19 @@ use App\Http\Controllers\ProductController;
 
 // Route::resource('v1/products',ProductController::class);
 
+//Protected Routes
+Route::group(['middleware'=>'auth:sanctum'], function () {
+    Route::put('v1/products/{product}',[ProductController::class,'update']);
+    Route::post('v1/products',[ProductController::class,'store']);
+    Route::delete('v1/products/{product}',[ProductController::class,'destroy']);
+});
+
+//Public Routes
 Route::get('v1/products',[ProductController::class,'index']);
-
-Route::get('v1/products/search/{name}',[ProductController::class,'search']);
-
-Route::post('v1/products',[ProductController::class,'store']);
 
 Route::get('v1/products/{product}',[ProductController::class,'show']);
 
-Route::put('v1/products/{product}',[ProductController::class,'update']);
-
-Route::delete('v1/products/{product}',[ProductController::class,'destroy']);
-
+Route::get('v1/products/search/{name}',[ProductController::class,'search']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
